4ecf794eafe42259cda2f1442e64ae49d341293d
Pete Fitton <petefitton@gmail.com>
2020-08-11 14:38:31 -0700

    Updates

---
+++ precache-manifest.7f2955a4175a30a95bb5e62f350ba04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdbd4f319870d7047b539c76e483d6d",
+    "url": "/student-randomizer/index.html"
+  },
+  {
+    "revision": "33c98a532c4cfda05677",
+    "url": "/student-randomizer/static/css/main.60114406.chunk.css"
+  },
+  {
+    "revision": "accee2085d75f59a0a96",
+    "url": "/student-randomizer/static/js/2.7934322f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/student-randomizer/static/js/2.7934322f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33c98a532c4cfda05677",
+    "url": "/student-randomizer/static/js/main.dc0e05d2.chunk.js"
+  },
+  {
+    "revision": "9342c9813b9dcaeb46d1",
+    "url": "/student-randomizer/static/js/runtime-main.c7dd1f8b.js"
+  }
+]);
