bb7d82ecda43a719cbb588b41d7c825c93db3de7
Pete Fitton <petefitton@gmail.com>
2020-08-11 20:27:31 -0700

    Update App.js & style.css
    Update BrowserRouter to have basename in App.js
    Create Link container for h1
    Remove underline from style.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import './style.css';
 import Main from './Main';
 import SEIR713 from './SEIR713';
@@ -8,8 +8,8 @@ import SEIR713 from './SEIR713';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <h1>Student Randomizer</h1>
+      <BrowserRouter basename="student-randomizer">
+        <Link to="/" className="headerLink"><h1>Student Randomizer</h1></Link>
         <Switch>
           <Route path="/SEIR-713" component={SEIR713} />
           <Route path="/" component={Main} />
